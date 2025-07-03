391fcacb193c687ee56f9402ef89da1fa3324650
misu <62200549+mihailnica10@users.noreply.github.com>
2025-07-03 17:03:50 +0000

    rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,4 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    },
-
-    imageService: "cloudflare"
-  })
 });
